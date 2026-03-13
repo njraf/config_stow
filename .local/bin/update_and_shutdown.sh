e87e53153ac36f8bcf78ced1ed2b43006beb26c5
Nick Rafala <nrafala@outlook.com>
2026-03-13 17:10:20 -0400

    tmux panes stay zoomed in when changing panes. Update and shutdown script should repeatedly try shutting down. clang-format columnLimit is now 120.

--- .local/bin/update_and_shutdown.sh
+++ .local/bin/update_and_shutdown.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-gnome-terminal -- sudo bash -c "dnf -y update && sleep 2 && shutdown -h now"
+
+# Add the following line to keyboard shortcuts:
+# gnome-terminal -- sudo bash -c '/home/nick/.local/bin/update_and_shutdown.sh' 
+
+dnf -y upgrade && sleep 2
+shutdown -h now
+while [ $? -ne 0 ]; do
+	echo "Could not shutdown. Will try again."
+	sleep 5
+	shutdown -h now
+done
+
