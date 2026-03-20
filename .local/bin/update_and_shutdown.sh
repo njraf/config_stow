3ff43df3d8842f84e8496ac547a0349d78d5d00f
Nick Rafala <nrafala@outlook.com>
2026-03-20 14:11:06 -0400

    The update and shutdown script now uses sudo

--- .local/bin/update_and_shutdown.sh
+++ .local/bin/update_and_shutdown.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Add the following line to keyboard shortcuts:
-# gnome-terminal -- sudo bash -c '/home/nick/.local/bin/update_and_shutdown.sh' 
+# gnome-terminal -- /home/nick/.local/bin/update_and_shutdown.sh
 
-dnf -y upgrade && sleep 2
-shutdown -h now
+sudo dnf -y upgrade
+sleep 2
+sudo shutdown -h now
 while [ $? -ne 0 ]; do
 	echo "Could not shutdown. Will try again."
 	sleep 5
-	shutdown -h now
+	sudo shutdown -h now
 done
 
