b9118f644e73cf9efb02f37f30d8b91445820151
Nick Rafala <njrafala@outlook.com>
2026-07-03 11:56:12 -0400

    Add homebrew kegs/casks to the PATH. Update the initialization script to install and login to dashlane and gh to clone config_stow automatically. Source OpenDDS setenv.sh if it exists.

--- dot-local/bin/initialize_environment.sh
+++ dot-local/bin/initialize_environment.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Used to set up the environment on a new install
 
+
 # prints warnings in yellow text
 function printWarning() {
     echo $'\e[93m'"$@"$'\e[0m'
@@ -10,6 +11,14 @@ function printWarning() {
 function printError() {
     echo $'\e[91m'"$@"$'\e[0m'
 }
+
+if [[ "$(whoami)" == "root" ]]; then
+	printError "This script must not be run as root"
+	exit 1
+fi
+
+# run this script from HOME
+pushd ~
 
 # determine which package manager the system uses and create an install string
 echo "Checking package manager"
@@ -43,9 +52,56 @@ for software in "${FP_TO_DOWNLOAD[@]}"; do
     flatpak install "$software"
 done
 
+# install lazyvim
+#NOTE: should be done before cloning config_stow so that changes from the stow can be applied
+echo "Installing LazyVim"
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
+# install dashlane from brew
+if ! which brew &> /dev/null; then
+	bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+	brew install -y dashlane/tap/dashlane-cli
+fi
+
+# check whether we are logged in to dashlane
+function loggedInToDashlane() {
+	if [[ "$(dcli status | cut -d ':' -f 2 | head -n 1)" =~ "no" ]]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+# log in to dashlane
+if ! loggedInToDashlane; then
+	dcli sync
+fi
+
+# log in to gh
+if which dcli &> /dev/null && which gh &> /dev/null && loggedInToDashlane; then
+	export GH_TOKEN="$(dcli note github-the-token)"
+	gh auth login -cp https -h GitHub.com # should automatically get the token from GH_TOKEN; not sure about user name
+fi
+
+# clear sensitive fields
+export GH_TOKEN=""
+dcli lock
+
+# clone repo and install dotfiles
+if [[ "$(basename $PWD)" != "config_stow" ]]; then
+	if ! gh repo clone config_stow; then
+		printError "Could not clone config_stow. "
+		popd
+		exit 1
+	fi
+	cd config_stow
+fi
 echo "Stowing"
 stow --dotfiles --adopt .
-git reset --hard
+git stash --include-untracked
+cd ~
 
 # determine the default terminal
 echo "Checking default terminal"
@@ -56,9 +112,9 @@ if which gnome-terminal &>/dev/null; then
     NEW_WINDOW="--window"
 elif which ptyxis &>/dev/null; then
     DEFAULT_TERMINAL="ptyxis"
-    NEW_WINDOW="--new-window"
+#NEW_WINDOW="--new-window"
 else
-    printWarning "Could not determine the default terminal. Not setting custom keybind to open terminal."
+    printWarning "Could not determine the default terminal. Not setting custom keybind to open terminal or update_and_shutdown."
 fi
 
 # gnome custom keyboard shortcuts
@@ -78,10 +134,11 @@ if [[ "GNOME" == "$XDG_SESSION_DESKTOP" ]] || [[ "gnome" == "$XDG_SESSION_DESKTO
     gsettings set "$KEYBIND_PREFIX"'0/' binding "'<Primary><Alt>m'"
     gsettings set "$KEYBIND_PREFIX"'0/' command "'bash $HOME/.local/bin/toggle_mute_focused_window.sh'"
 
-    gsettings set "$KEYBIND_PREFIX"'1/' name "'Update and Shutdown'"
-    gsettings set "$KEYBIND_PREFIX"'1/' binding "'<Primary><Alt><Super>x'"
-    gsettings set "$KEYBIND_PREFIX"'1/' command "'$DEFAULT_TERMINAL $NEW_WINDOW -- $HOME/.local/bin/update_and_shutdown.sh'"
     if [[ -n "$DEFAULT_TERMINAL" ]]; then
+        gsettings set "$KEYBIND_PREFIX"'1/' name "'Update and Shutdown'"
+	gsettings set "$KEYBIND_PREFIX"'1/' binding "'<Primary><Alt><Super>x'"
+	gsettings set "$KEYBIND_PREFIX"'1/' command "'$DEFAULT_TERMINAL $NEW_WINDOW -- $HOME/.local/bin/update_and_shutdown.sh'"
+
         gsettings set "$KEYBIND_PREFIX"'2/' name "'New Terminal'"
         gsettings set "$KEYBIND_PREFIX"'2/' binding "'<Super>t'"
         gsettings set "$KEYBIND_PREFIX"'2/' command "'$DEFAULT_TERMINAL $NEW_WINDOW'"
@@ -90,9 +147,7 @@ else
     printWarning "Could not determine the Desktop Environment. Not setting keyboard shortcuts."
 fi
 
-# install lazyvim
-echo "Installing LazyVim"
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
+source ~/.bashrc
 
+popd
 echo "DONE"
